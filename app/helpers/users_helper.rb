9561fa40c39895c68c3ae5f715f39b2b287a1334
joalorro <joalorro@gmail.com>
2018-08-23 12:09:57 -0400

    added some back buttons to phrase/ new & edit and downloaded bootstrap

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  class NewUserValidator < ActiveModel::Validator
+    def validate user
+      if !!user.password && user.password.length < 6
+        user.errors.add(:password, "Password must be greater than 6 characters long!")
+      end
+
+      if !user.username && user.username.length < 6
+        user.errors.add(:username, "Username must be greater than 6 characters long!")
+      end
+
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with UsersHelper::NewUserValidator
+
   has_many :phrases
   has_many :comments
   has_many :ratings
   has_many :states, through: :phrases
-  validates_confirmation_of :password
-  attr_accessor :password_confirmation
   has_secure_password
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :password, presence: true
+  validates_confirmation_of :password
+  validates_presence_of :password_confirmation, :message => "Confirmation should match password"
+
+  attr_accessor :password_confirmation
 end
